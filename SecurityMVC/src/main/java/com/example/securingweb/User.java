895102f90977739ca2a359ecc6e6c080bf2667a0
zqweng <53497427+zqweng@users.noreply.github.com>
2024-02-01 00:22:29 -0800

    add mysql support for SecurityMVC project

---
+++ SecurityMVC/src/main/java/com/example/securingweb/User.java
@@ -0,0 +1,37 @@
+package com.example.securingweb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	@Id
+    private Long id;
+    private String username;
+    private String password;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    // No need to define explicit constructors here
+}
--- SecurityMVC/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ SecurityMVC/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -1,19 +1,25 @@
 package com.example.securingweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
@@ -29,16 +35,26 @@ public class WebSecurityConfig {
 
 		return http.build();
 	}
-
+	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		//provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+	    provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
 	}
+	
+	/*
+	 * @Bean public UserDetailsService userDetailsService() { UserDetails user =
+	 * User.withDefaultPasswordEncoder() .username("user") .password("password")
+	 * .roles("USER") .build();
+	 * 
+	 * return new InMemoryUserDetailsManager(user); }
+	 * 
+	 */	 @Bean
+	 public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		 return new BCryptPasswordEncoder();
+	 }
+	
 }
